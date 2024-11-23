22ddfcf4d899248dad8658e010b88235228c5b99
kithurshika Kirushnan <kirushnankithurshika@gmail.com>
2024-11-23 07:36:50 +0530

    footer finished

--- dt_energy_website/src/components/Contact/contact.jsx
+++ dt_energy_website/src/components/Contact/contact.jsx
@@ -1,14 +1,104 @@
 import React from 'react';
 import './contact.css';
+import SLS from '../../assets/SLS.png';
+import CEB from '../../assets/CEB.png';
+import callL from '../../assets/call.png';
+import whatsapp from '../../assets/whatsapp.png';
+import website from '../../assets/website.png';
+import facebook from '../../assets/facebook.png';
+import emailL from '../../assets/email.png';
+import locationL from '../../assets/location.png';
+import whitelogo from '../../assets/whitelogo.png';
 
 function Contact() {
   return (
     <div className="contact-container">
-      <div className='contact details'>
+      <div className="Authorized-container">
+        <div className='contact-heading'>Authorized by</div>
+        <div className="authorized-logos">
+          <img src={SLS} alt="SLS Logo" />
+          <img src={CEB} alt="CEB Logo" />
+        </div>
+      </div>
+      <div className="contact-details">
+        <div className="contact-info">
+          <div className="contact-heading">Contact Us</div>
+
+          <div className="contact-detailP">
+            <span className="icon-container">
+              <img src={locationL} alt="Location Icon" />
+            </span>
+            <strong>Address:</strong>
+            <span className="detail-text">24/8, Green Road,Trincomalee.</span>
+          </div>
+
+          <div className="contact-detailP">
+            <span className="icon-container">
+              <a href="tel:+94772436082">
+                <img src={callL} alt="Phone Icon" />
+              </a>
+            </span>
+            <strong>Phone:</strong>
+            <span className="detail-text">
+              <a href="tel:+94772436082">+94772436082</a>
+            </span>
+          </div>
+
+          <div className="contact-detailP">
+            <span className="icon-container">
+              <a href="mailto:dreamtrillions100@gmail.com">
+                <img src={emailL} alt="Email Icon" />
+              </a>
+            </span>
+            <strong>Email:</strong>
+            <span className="detail-text">
+              <a href="mailto:dreamtrillions100@gmail.com">dreamtrillions100@gmail.com</a>
+            </span>
+          </div>
+
+        </div>
+      </div>
+
+
+
+
+      <div class="container">
+        <h2>Want to be a partner with us?</h2>
+        <p>If you're interested in our partnership and would like to find out some more information, one of our advisors is excited to help.</p>
+        <button class="cta-button">Get Started</button>
+      </div>
+      {/* Last Column: White Logo */}
+      <div className="white-logocontainer">
+        <div className="white-logo">
+          <img src={whitelogo} alt="White Logo" />
+        </div>
+      </div>
+      <div className="fbwhatsappweb">
+        <div className="icon-container">
+          <a
+            href="https://web.facebook.com/profile.php?id=61565691174954"
+
+          >
+            <img src={facebook} alt="Facebook Icon" />
+          </a>
+        </div>
+        <div className="icon-container">
+          <a
+            href="https://wa.me/94772436082"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={whatsapp} alt="WhatsApp Icon" />
+          </a>
+        </div>
+        <div className="icon-container">
+          <img src={website} alt="Website Icon" />
+        </div>
 
       </div>
-      <div className='contact partner'></div>
-      <div className='white logo'></div>
+      <div class="middle-content">
+        <p className='rights'>All rights reserved to Dream Trillions (Pvt) Ltd</p>
+      </div>
     </div>
   );
 }
